e992f3778f3903ea868660b97233207c1bb3466f
temaniall <159032430+temaniall@users.noreply.github.com>
2026-03-06 01:48:36 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,18 @@
-while true; do
-    STATUS=$(curl -s -o /dev/null -w "%{http_code}" -L https://www.altstu.ru/)
-    LOG="/tmp/mysql/access.log"
-    if [ "$STATUS" -eq 200 ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - SUCCESS (Status: $STATUS)" >> $LOG
-    else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - FAILED (Status: $STATUS)" >> $LOG
-    fi
-    sleep 30
-done
+DIR="/tmp/mysql"
+PHP="$DIR/index.php"
+SH="$DIR/script.sh"
+LOG="$DIR/logi.log"
+GIT_PHP="https://raw.githubusercontent.com/temaniall/lb4/main/lab4.php"
+
+if [ ! -f $PHP ]; then
+    curl -s $GIT_PHP -o $PHP
+fi
+
+if [ ! -f $LOG ]; then touch $LOG; fi
+COUNT=$(wc -l < $LOG)
+
+if [ $COUNT -lt 10 ]; then
+    curl -s -o /dev/null https://www.altstu.ru/
+    NEXT=$((COUNT + 1))
+    echo "$NEXT успешно" >> $LOG
+fi
